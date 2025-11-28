09c15a3b41f56d72a799490a9aecd1fd4cc16fe5
ZhiXia <zhixiaovo@gmail.com>
2025-11-28 16:42:05 +0800

    Project setup

---
+++ README.md
@@ -0,0 +1,4 @@
+
+Reminex is a application for indexing some files into db, and then search files fastly.
+
+It's usually used when you need to access a network driver, which is very slow to search, and files may change slowly. In this case, this app works suitablely.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use clap::{Parser, Subcommand};
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::parse();
+    match app.commands {
+        Commands::Index | Commands::I => {
+
+        },
+
+        Commands::Search | Commands::S => {
+
+        }
+    }
+}
+
+#[derive(Parser)]
+struct App {
+
+    #[arg(help="Input the path of the database, or it will default use './' as position.")]
+    db: Option<String>,
+    
+    #[command(subcommand)]
+    commands: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+
+    #[command(about="Start indexing files.")]
+    Index,
+
+    #[command(about="Start indexing files.")]
+    I,
+
+    #[command(about="Search through database.")]
+    Search,
+
+    #[command(about="Search through database.")]
+    S,
 }
